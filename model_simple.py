d217d5e0ae61402112267d57b4b8e817cdf7d562
Paryush Jain <paryushj1999@gmail.com>
2026-07-06 14:45:14 +0530

    Commits
    commits

--- model_simple.py
+++ model_simple.py
@@ -3,6 +3,22 @@ import numpy as np
 def inverse(matrix):
     return np.linalg.inv(matrix)
 
+class method:
+    def __init__(self):
+        pass
+
+    def step(self, x):
+        if x<=0:
+            return 0
+        else:
+            return 1
+        
+    def relu(self, x):
+        return max(0,x)
+    
+    def linear(self,x):
+        return x
+    
 def train_test_split(data, test_size=0.2):
     np.random.seed(42)
     shuffled_indices = np.random.permutation(len(data))
@@ -31,6 +47,8 @@ class linear_regression:
         mse = np.mean((y_test - y_pred)**2)
         rmse = np.sqrt(mse)
         r2 = 1 - (np.sum((y_test - y_pred)**2) / np.sum((y_test - np.mean(y_test))**2))
+        accuracy = np.mean((y_test - y_pred)**2 < 0.01)
+        print("Accuracy:", accuracy)
         print("Mean Squared Error:", mse)
         print("Root Mean Squared Error:", rmse)
         print("R-squared:", r2)
@@ -38,27 +56,25 @@ class linear_regression:
     def predict(self,x,m,c):
         return m*x + c
 
-class polynomial_regression:
-    def __init__(self, train_data, test_data, degree):
+class logistic_regression:
+    def __init__(self,train_data, test_data, threshold=0.5):
         self.train_data = train_data
         self.test_data = test_data
-        self.degree = degree
-    
-    def train(self, x_train, y_train):
-        x = self.train_data['x']
-        y = self.train_data['y']
-        b = inverse(x.T*x) * x.T * y
-        e = y - x*b
-        return b, e
+        self.threshold = threshold
 
-    def test(self, b, x_test, y_test):
-        y_pred = self.predict(x_test, b)
+    def pred(self,x_train,Z):
+        z = Z(x_train)
+        y_pred = 1 / (1 + np.exp(-z))
+        return y_pred
+    
+    def test(self,x_test, y_test):
+        y_pred = self.pred(x_test, self.Z)
+        y_pred_class = (y_pred >= self.threshold).astype(int)
+        accuracy = np.mean(y_pred_class == y_test)
         mse = np.mean((y_test - y_pred)**2)
         rmse = np.sqrt(mse)
         r2 = 1 - (np.sum((y_test - y_pred)**2) / np.sum((y_test - np.mean(y_test))**2))
+        print("Accuracy:", accuracy)
         print("Mean Squared Error:", mse)
         print("Root Mean Squared Error:", rmse)
         print("R-squared:", r2)
-
-    def predict(self, x, b):
-        return x*b
--- test.py
+++ test.py
@@ -2,16 +2,18 @@ import model_simple as model
 import pandas as pd
 import numpy as np
 
-train_data = pd.read_csv('archive(1)/train.csv')
-test_data = pd.read_csv('archive(1)/test.csv')
+train_data = pd.read_csv('linear_reg_data/train.csv')
+test_data = pd.read_csv('linear_reg_data/test.csv')
 
-regression_model = model.linear_regression(train_data, test_data)
-m,c = regression_model.train(train_data['x'], train_data['y'])
-regression_model.test(m, c, test_data['x'], test_data['y'])
+# regression_model = model.linear_regression(train_data, test_data)
+# m,c = regression_model.train(train_data['x'], train_data['y'])
+# regression_model.test(m, c, test_data['x'], test_data['y'])
 
-data = pd.read_csv('/home/user/Projects/AI/Ice_cream selling data.csv')
-train_data, test_data = model.train_test_split(data, test_size=0.2)
-test_data = test_data.reset_index(drop=True)
-regression_model_poly = model.polynomial_regression(train_data, test_data, degree=2)
-b, e = regression_model_poly.train(train_data['Temperature (°C)'], train_data['Ice Cream Sales (units)'])
-regression_model_poly.test(b, test_data['Temperature (°C)'], test_data['Ice Cream Sales (units)'])
+# data = pd.read_csv('linear_reg_data/data.csv')
+# train_data, test_data = model.train_test_split(data, test_size=0.2)
+model = model.polynomial_regression(train_data, test_data, degree=2)
+x,b = model.train(train_data['x'], train_data['y'])
+print("Coefficients:", b)
+
+
+print(data.head())
